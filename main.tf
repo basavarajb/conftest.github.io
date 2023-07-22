e8f95b0c238fbdcd0af10efb9b2bb9fb4a008177
basavarajb <kumarbiradar7@gmail.com>
2023-07-22 13:43:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,16 @@ terraform {
     }
   }
 }
-
+provider "azurerm" {
+  client_id="60c3c76d-0e15-47d0-b57f-f83246268dc5"
+subscription_id="fd7d53ef-e290-4ab1-937e-fec061c00132"
+tenant_id="7a32773c-a86e-490d-ae40-5780e4791a65"
+client_secret="I-R8Q~iof_.aW_ITVAg2CD9nL0kwyigGeao_Ddb1"
+}
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
 output "instance_ip_addr" {
   value       = "HI"
   description = "The private IP address of the main server instance."
